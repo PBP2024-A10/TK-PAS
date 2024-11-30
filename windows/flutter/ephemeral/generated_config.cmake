372fe4644e4d10d8175afc2110b44b469730fbe9
anggunsdewi <4n66und3w1@gmail.com>
2024-11-30 17:30:10 +0700

    masih raw

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\anggu\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\anggu\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS"
+  "FLUTTER_ROOT=C:\\Users\\anggu\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS"
+  "FLUTTER_TARGET=C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\anggu\\OneDrive - UNIVERSITAS INDONESIA\\coolyeah\\term 3\\PBP\\TK-PAS\\.dart_tool\\package_config.json"
+)
